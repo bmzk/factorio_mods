e49c2c640e2245ec985add2a2ac89b1be30236a1
bmzk <37799175+bmzk@users.noreply.github.com>
2020-01-12 08:41:17 +0800

    0

--- fpc_Game_settings/fpc_game_settings_0.0.4/data.lua
+++ fpc_Game_settings/fpc_game_settings_0.0.4/data.lua
@@ -1,5 +1,5 @@
 
-data.raw["accumulator"]["accumulator"]["energy_source"].buffer_capacity="5000GJ"
+data.raw["accumulator"]["accumulator"]["energy_source"].buffer_capacity="3000GJ"
 data.raw["accumulator"]["accumulator"]["energy_source"].input_flow_limit = "300MW"
 data.raw["accumulator"]["accumulator"]["energy_source"].output_flow_limit = "300MW"
 --插件效果分享塔
@@ -8,7 +8,10 @@ data.raw["beacon"].beacon.module_specification.module_slots=8
 data.raw["beacon"].beacon.distribution_effectivity=10
 data.raw["beacon"].beacon.energy_usage = "5000kW"
 --插件
-data.raw.module["speed-module"].effect = { speed = {bonus = 2}, consumption = {bonus = 4}}
+data.raw.module["speed-module"].effect = { 
+  speed = {bonus = 2}, 
+  consumption = {bonus = 4},
+  pollution = {bonus = 4}}
 ----电炉
 data.raw["furnace"]["electric-furnace"].module_specification.module_slots=8
 
