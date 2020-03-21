806dd04685fc53dc7e9190a483ac4689227eafc4
bmzk <37799175+bmzk@users.noreply.github.com>
2020-03-21 19:20:41 +0800

    0

---
+++ Laser_Turret/fpc_laser_turret_0.0.3/data.lua
@@ -0,0 +1,5 @@
+
+
+data.raw["electric-turret"]["laser-turret"].max_health =50000
+data.raw["electric-turret"]["laser-turret"]["attack_parameters"].range=65
+data.raw["wall"]["stone-wall"].max_health =5000
